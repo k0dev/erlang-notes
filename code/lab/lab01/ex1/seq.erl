1a5bfc93ae2d685e383ea35233e0caef996a702c
k0dev <108899333+k0dev@users.noreply.github.com>
2022-11-20 23:22:04 +0100

    is_an_anagram and his test

--- code/lab/lab01/ex1/seq.erl
+++ code/lab/lab01/ex1/seq.erl
@@ -1,8 +1,12 @@
 -module(seq).
--export([is_palindrome/1]).
+-export([is_palindrome/1, is_an_anagram/2]).
 
 is_letter(C) -> (C >= $a) and (C =< $z) or (C >= $A) and (C =< $Z).
 
 is_palindrome(S) -> 
   L = lists:filter(fun is_letter/1, S),
   string:equal(L, lists:reverse(L), true).  % string:equal(A, B, IgnoreCase) -> boolean()
+
+is_an_anagram(S, Dict) ->
+  SS = lists:sort(S),
+  lists:any(fun(Candidate) -> SS =:= lists:sort(Candidate) end, Dict).
--- code/lab/lab01/ex1/seq_test.erl
+++ code/lab/lab01/ex1/seq_test.erl
@@ -1,9 +1,17 @@
 -module(seq_test).
--export([is_palindrome_test/0]).
+-export([is_palindrome_test/0, is_an_anagram_test/0]).
 
 is_palindrome_test() ->
   Inputs = ["detartrated", "Do geese see God?","Rise to vote, sir.", "random", "aNna", "test"],
   lists:foreach(fun(S) -> 
                     io:format("is ~p palindrome? ~p~n", [S, seq:is_palindrome(S)]) 
                 end, 
+                Inputs).
+
+is_an_anagram_test() ->
+  Dict = ["jioawd", "oawijaw", "iwad", "anna", "knee"],
+  Inputs = ["nana", "keen", "ciao", "test"],
+  lists:foreach(fun(S) ->
+                    io:format("is ~p an anagram? ~p~n", [S, seq:is_an_anagram(S, Dict)])
+                end,
                 Inputs).
