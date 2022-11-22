8e2aed887f3b43f790cad98454c3aab6c5d7fc6b
k0dev <108899333+k0dev@users.noreply.github.com>
2022-11-22 17:43:45 +0100

    fixed bug and added debug prints

--- code/lab/lab01/ex2/expressions.erl
+++ code/lab/lab01/ex2/expressions.erl
@@ -1,8 +1,7 @@
 -module(expressions).
 -export([parse/1, evaluate/1]).
 
-% assumo che ogni token sia al massimo un carattere (anche i numeri)
-
+%% assumo che ogni token sia al massimo un carattere (anche i numeri) %%
 
 %% Evaluation %%
 
@@ -15,8 +14,8 @@ evaluate({invert, A})    -> -evaluate(A).
 
 %% Parsing %%
 
-% uso https://en.wikipedia.org/wiki/Shunting_yard_algorithm per trasformare
-% un espressione comune parentesizzata in una in notazione polacca
+% Shunting yard algorithm (https://en.wikipedia.org/wiki/Shunting_yard_algorithm):
+% per trasformare un espressione aritmetica in una in notazione polacca inversa 
 
 next_token([]) -> end_of_expr;
 next_token([H|T]) -> {H, T}.
@@ -26,22 +25,44 @@ create_expression([$+|T], [A,B|Acc]) -> create_expression(T, [{plus, B, A} | Acc
 create_expression([$-|T], [A,B|Acc]) -> create_expression(T, [{minus, B, A} | Acc]);
 create_expression([$/|T], [A,B|Acc]) -> create_expression(T, [{divide, B, A} | Acc]);
 create_expression([$*|T], [A,B|Acc]) -> create_expression(T, [{times, B, A} | Acc]);
-create_expression([$~|T], [A|Acc]) -> create_expression(T, [{invert, A} | Acc]);
-create_expression([H|T], Acc) -> create_expression(T, [{num, list_to_integer([H])} | Acc]).
+create_expression([$~|T], [A|Acc])   -> create_expression(T, [{invert, A} | Acc]);
+create_expression([H|T], Acc)        -> create_expression(T, [{num, list_to_integer([H])} | Acc]).
   
-
 parse(Str) -> create_expression(parse(next_token(Str), [], []), []).
 
-parse(end_of_expr, Output, Operators) -> lists:reverse(lists:append(Operators, Output));
-parse({Token, Rest}, Output, Operators) when 
-    Token >= $0, Token =< $9 -> parse(next_token(Rest), [Token|Output], Operators);
-parse({$(, Rest}, Output, Operators) -> parse(next_token(Rest), Output, [$(|Operators]);
-parse({$), Rest}, Output, Operators) -> parse_close_par(Rest, Output, Operators);
-parse({Op1, Rest}, Output, [Op2|T]) when
-    Op2 =/= $(, not (((Op1 =:= $*) or (Op1 =:= $/)) and ((Op2 =:= $+) or (Op2 =:= $-))) -> % per la precedenza degli operatori
-      parse({Op1, Rest}, [Op2|Output], T);
-parse({Op, Rest}, Output, Operators) -> parse(next_token(Rest), Output, [Op|Operators]).
+% caso base, abbiamo finito di parsare la stringa
+parse(end_of_expr, Output, Operators) -> lists:append(lists:reverse(Output), Operators); 
 
-parse_close_par(Rest, Output, [$(|T]) -> parse(next_token(Rest), Output, T);
-parse_close_par(Rest, Output, [H|T])  -> parse_close_par(Rest, [H|Output], T);
+% il prossimo token è una cifra -> lo aggiungo all'output
+parse({Token, Rest}, Output, Operators) when 
+    Token >= $0, Token =< $9 -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse(next_token(Rest), [Token|Output], Operators);
+
+% il prossimo token è una parentesi aperta -> la aggiungo agli operatori
+parse({$(, Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse(next_token(Rest), Output, [$(|Operators]);
+
+% il prossimo token è una parentesi chiusa -> 
+%         while the operator at the top of the operator stack is not a left parenthesis:
+%            {assert the operator stack is not empty}
+%            /* If the stack runs out without finding a left parenthesis, then there are mismatched parentheses. */
+%            pop the operator from the operator stack into the output queue
+%         pop the left parenthesis from the operator stack and discard it
+parse({$), Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse_close_par(Rest, Output, Operators);
+
+% il prossimo token è un operatore o1 ->
+%         while (
+%            there is an operator o2 other than the left parenthesis at the top
+%            of the operator stack, and (o2 has greater precedence than o1
+%            or they have the same precedence and o1 is left-associative)
+%          ):
+%            pop o2 from the operator stack into the output queue
+parse({Op1, Rest}, Output, [Op2|T]) when
+    Op2 =/= $(, not(((Op1 =:= $*) or (Op1 =:= $/)) and ((Op2 =:= $+) or (Op2 =:= $-))) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, [Op2|T]]), parse({Op1, Rest}, [Op2|Output], T);
+% push o1 onto the operator stack
+parse({Op, Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse(next_token(Rest), Output, [Op|Operators]).
+
+% pop the left parenthesis from the operator stack and discard it
+parse_close_par(Rest, Output, [$(|T]) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, [$(|T]]), parse(next_token(Rest), Output, T);
+% pop the operator from the operator stack into the output queue
+parse_close_par(Rest, Output, [H|T])  -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, [H|T]]), parse_close_par(Rest, [H|Output], T);
+% operator stack is empty -> mismatched parentheses
 parse_close_par(_, _, []) -> exit(unbalanced_par).
