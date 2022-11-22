80c8b5e9ea450351a481685e98797903c03d6b05
k0dev <108899333+k0dev@users.noreply.github.com>
2022-11-22 17:46:41 +0100

    version without debug prints

--- code/lab/lab01/ex2/expressions.erl
+++ code/lab/lab01/ex2/expressions.erl
@@ -27,18 +27,26 @@ create_expression([$/|T], [A,B|Acc]) -> create_expression(T, [{divide, B, A} | A
 create_expression([$*|T], [A,B|Acc]) -> create_expression(T, [{times, B, A} | Acc]);
 create_expression([$~|T], [A|Acc])   -> create_expression(T, [{invert, A} | Acc]);
 create_expression([H|T], Acc)        -> create_expression(T, [{num, list_to_integer([H])} | Acc]).
-  
+ 
 parse(Str) -> create_expression(parse(next_token(Str), [], []), []).
 
-% caso base, abbiamo finito di parsare la stringa
-parse(end_of_expr, Output, Operators) -> lists:append(lists:reverse(Output), Operators); 
+% caso base, abbiamo finito di parsare la stringa ->
+%     while there are tokens on the operator stack:
+%       /* If the operator token on the top of the stack is a parenthesis, then there are mismatched parentheses. */
+%       {assert the operator on top of the stack is not a (left) parenthesis}
+%       pop the operator from the operator stack onto the output queue
+parse(end_of_expr, Output, Operators) -> 
+  case lists:member($(, Operators) of
+    false -> lists:append(lists:reverse(Output), Operators);
+    true  -> exit(unbalanced_par)
+  end;
 
 % il prossimo token è una cifra -> lo aggiungo all'output
 parse({Token, Rest}, Output, Operators) when 
-    Token >= $0, Token =< $9 -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse(next_token(Rest), [Token|Output], Operators);
+    Token >= $0, Token =< $9 -> parse(next_token(Rest), [Token|Output], Operators);
 
 % il prossimo token è una parentesi aperta -> la aggiungo agli operatori
-parse({$(, Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse(next_token(Rest), Output, [$(|Operators]);
+parse({$(, Rest}, Output, Operators) -> parse(next_token(Rest), Output, [$(|Operators]);
 
 % il prossimo token è una parentesi chiusa -> 
 %         while the operator at the top of the operator stack is not a left parenthesis:
@@ -46,7 +54,7 @@ parse({$(, Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators q
 %            /* If the stack runs out without finding a left parenthesis, then there are mismatched parentheses. */
 %            pop the operator from the operator stack into the output queue
 %         pop the left parenthesis from the operator stack and discard it
-parse({$), Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse_close_par(Rest, Output, Operators);
+parse({$), Rest}, Output, Operators) -> parse_close_par(Rest, Output, Operators);
 
 % il prossimo token è un operatore o1 ->
 %         while (
@@ -56,13 +64,15 @@ parse({$), Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators q
 %          ):
 %            pop o2 from the operator stack into the output queue
 parse({Op1, Rest}, Output, [Op2|T]) when
-    Op2 =/= $(, not(((Op1 =:= $*) or (Op1 =:= $/)) and ((Op2 =:= $+) or (Op2 =:= $-))) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, [Op2|T]]), parse({Op1, Rest}, [Op2|Output], T);
+    Op2 =/= $(, not(((Op1 =:= $*) or (Op1 =:= $/)) and ((Op2 =:= $+) or (Op2 =:= $-))) -> 
+        parse({Op1, Rest}, [Op2|Output], T);
+
 % push o1 onto the operator stack
-parse({Op, Rest}, Output, Operators) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, Operators]), parse(next_token(Rest), Output, [Op|Operators]).
+parse({Op, Rest}, Output, Operators) -> parse(next_token(Rest), Output, [Op|Operators]).
 
 % pop the left parenthesis from the operator stack and discard it
-parse_close_par(Rest, Output, [$(|T]) -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, [$(|T]]), parse(next_token(Rest), Output, T);
+parse_close_par(Rest, Output, [$(|T]) -> parse(next_token(Rest), Output, T);
 % pop the operator from the operator stack into the output queue
-parse_close_par(Rest, Output, [H|T])  -> io:format("Output stack: ~p~nOperators queue: ~p~n", [Output, [H|T]]), parse_close_par(Rest, [H|Output], T);
+parse_close_par(Rest, Output, [H|T])  -> parse_close_par(Rest, [H|Output], T);
 % operator stack is empty -> mismatched parentheses
 parse_close_par(_, _, []) -> exit(unbalanced_par).
