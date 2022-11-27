af00f45c2041cbadbe112074cd288c2e3869a9c4
k0dev <108899333+k0dev@users.noreply.github.com>
2022-11-27 19:28:29 +0100

    simple message sending without debug prints

--- code/lab/lab01/ex3/centralized_process_ring.erl
+++ code/lab/lab01/ex3/centralized_process_ring.erl
@@ -1,6 +1,17 @@
 -module(centralized_process_ring).
 -export([start/3]).
 
+start(M, N, Message) -> 
+  create(N),
+  send_messages(M, Message),
+  ok.
+
+send_messages(0, _) -> ok;
+send_messages(M, Message) ->
+  first_node ! Message,
+  send_messages(M-1, Message),
+  first_node ! stop.
+
 create(N) ->
   Pid = spawn(fun() -> ring_node(first_node) end),
   io:format("[master] created node ~p with pid ~p chained to node 1 (first_node, not yet created)~n", [N, Pid]),
@@ -14,15 +25,17 @@ create(NextNode, N) ->
   io:format("[master] created node ~p with pid ~p chained to node ~p with pid ~p~n", [N, Pid, N+1, NextNode]),
   create(Pid, N-1).
 
-start(_M, N, _Message) -> create(N).
-
 ring_node(NextNode) ->
   receive
     stop ->
       debug_print("stop", NextNode),
       % uso catch per evitare un eccezione badarg nel momento in cui NextNode == first_node
       catch NextNode ! stop,
-      debug_print("stopping")
+      debug_print("stopping");
+    Msg ->
+      debug_print(Msg, NextNode),
+      catch NextNode ! Msg,
+      ring_node(NextNode)
   end.
 
 debug_print(Msg, To) -> io:format("[From: ~p] -> [To: ~p] - ~p~n", [self(), To, Msg]).
