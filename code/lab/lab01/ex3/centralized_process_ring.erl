ac27897dc65852f85fb4c210b052f859a74b6d46
k0dev <108899333+k0dev@users.noreply.github.com>
2022-11-27 20:04:25 +0100

    add forwarding count tracking and message indexes for better debug

--- code/lab/lab01/ex3/centralized_process_ring.erl
+++ code/lab/lab01/ex3/centralized_process_ring.erl
@@ -1,16 +1,26 @@
 -module(centralized_process_ring).
 -export([start/3]).
 
+% A quale processo collego il primo nodo creato? Ad un atomo che registerò in futuro.
+% (con collegare non si intende un link di erlang, ma piuttosto un collegamento simile a quello delle linked list)
+% P_n   ---> first_node (non ancora spawnato)
+% P_n-1 ---> P_n
+% ..
+% P_1   ---> P_2
+% register(first_node, P_1)
+% quindi l'anello si completa.
+
 start(M, N, Message) -> 
-  create(N),
-  send_messages(M, Message),
+  create(N),                      % creo l'anello
+  send_messages(M, {1, Message}), % introduco i messaggi
+  timer:sleep(10),                % aspetto un po' per farli circolare
+  first_node ! stop,              % mando il messaggio di stop
   ok.
 
 send_messages(0, _) -> ok;
-send_messages(M, Message) ->
-  first_node ! Message,
-  send_messages(M-1, Message),
-  first_node ! stop.
+send_messages(M, {Index, Message}) ->
+  first_node ! {0, Index, Message},
+  send_messages(M-1, {Index+1, Message}).
 
 create(N) ->
   Pid = spawn(fun() -> ring_node(first_node) end),
@@ -28,26 +38,18 @@ create(NextNode, N) ->
 ring_node(NextNode) ->
   receive
     stop ->
-      debug_print("stop", NextNode),
-      % uso catch per evitare un eccezione badarg nel momento in cui NextNode == first_node
-      catch NextNode ! stop,
-      debug_print("stopping");
-    Msg ->
-      debug_print(Msg, NextNode),
-      catch NextNode ! Msg,
-      ring_node(NextNode)
+        debug_print("stop", NextNode),
+        % uso catch per evitare un eccezione badarg nel momento in cui NextNode == first_node
+        catch NextNode ! stop,
+        debug_print("stopping"),
+        exit(stop);
+    {ForwardedCount, Index, Message} ->
+        debug_print(ForwardedCount, Index, Message, NextNode),
+        catch NextNode ! {ForwardedCount+1, Index, Message},
+        ring_node(NextNode)
   end.
 
+debug_print(Count, Index, Msg, To) -> 
+  io:format("[From: ~p] -> [To: ~p] - ~p:~p (forwarded ~p times)~n", [self(), To, Index, Msg, Count]).
 debug_print(Msg, To) -> io:format("[From: ~p] -> [To: ~p] - ~p~n", [self(), To, Msg]).
 debug_print(Msg) -> io:format("[Pid: ~p] ~p~n", [self(), Msg]).
-
-% Esempio output creazione:
-% 1> c(centralized_process_ring).
-% {ok,centralized_process_ring}
-% 2> centralized_process_ring:start(0, 5, x).
-% [master] created node 5 with pid <0.89.0> chained to node 1 (first_node, not yet created)
-% [master] created node 4 with pid <0.90.0> chained to node 5 with pid <0.89.0>
-% [master] created node 3 with pid <0.91.0> chained to node 4 with pid <0.90.0>
-% [master] created node 2 with pid <0.92.0> chained to node 3 with pid <0.91.0>
-% [master] created node 1 (first_node) with pid <0.93.0> chained to node 2 with pid <0.92.0>
-% ok
