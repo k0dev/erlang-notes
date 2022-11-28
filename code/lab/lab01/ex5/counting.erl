4db5a76d20ff7f956dc6ed1dea8202d3321b848c
k0dev <108899333+k0dev@users.noreply.github.com>
2022-11-28 19:07:43 +0100

    improve solution

--- code/lab/lab01/ex5/counting.erl
+++ code/lab/lab01/ex5/counting.erl
@@ -1,35 +1,55 @@
 -module(counting).
 -export([start/0, rpc/2, loop/1]).
 
--define(zero(I), {I, 0}).
+% > c(counting).
+% {ok,counting}
+% > Server = counting:start().
+% <0.89.0>
+% counting:rpc(Server, dummy1).
+% "You called dummy1"
+% > counting:rpc(Server, dummy1).
+% "You called dummy1"
+% > counting:rpc(Server, dummy3).
+% "You called dummy3"
+% > counting:rpc(Server, dummy3).
+% "You called dummy3"
+% > counting:rpc(Server, dummy3).
+% "You called dummy3"
+% > counting:rpc(Server, dummy3).
+% "You called dummy3"
+% > counting:rpc(Server, tot).   
+% [{tot,1},{dummy1,2},{dummy2,0},{dummy3,4}]
 
-update_count([{S, C}|T], S) -> [{S, C+1}|T];
+
+% Incrementa di uno il contatore in corrispondenza della chiave specificata
+update_count([{S, C}|T], S) -> [{S, C+1}|T];             % matcha se le chiavi (S) sono uguali
 update_count([H|T], S)      -> [H | update_count(T, S)].
 
-start() -> spawn(counting, loop, [[?zero(tot), ?zero(dummy1), ?zero(dummy2), ?zero(dummy3)]]).
+% Avvia il server e lo inizializza con i contatori a 0 per ogni servizio
+start() -> spawn(counting, loop, [[{tot, 0}, {dummy1, 0}, {dummy2, 0}, {dummy3, 0}]]). 
 
-
+% Wrapper per il client
 rpc(Pid, Service) ->
 	Pid ! {self(), Service},
 	receive
-		Msg -> Msg
-	after(5000) -> exit("RPC failed")
+		{Pid, Answer} -> Answer % prendiamo solo i messaggi inviati dal server (match su Pid)
+	after(5000) -> exit(timeout)
 	end.
 
-
+% Server loop
 loop(L) ->
 	receive
-		{Pid, tot}    -> 
+		{Pid, tot}    ->
 			Updated = update_count(L, tot),
-			Pid ! Updated,
+			Pid ! {self(), Updated},
 			loop(Updated);
 		{Pid, dummy1} -> dummy1(Pid), loop(update_count(L, dummy1));
 		{Pid, dummy2} -> dummy2(Pid), loop(update_count(L, dummy2));
 		{Pid, dummy3} -> dummy3(Pid), loop(update_count(L, dummy3))
 	end.
 
-
-dummy1(Pid) -> Pid ! "You called dummy1".
-dummy2(Pid) -> Pid ! "You called dummy2".
-dummy3(Pid) -> Pid ! "You called dummy3".
+% Servizi offerti dal server
+dummy1(Pid) -> Pid ! {self(), "You called dummy1"}.
+dummy2(Pid) -> Pid ! {self(), "You called dummy2"}.
+dummy3(Pid) -> Pid ! {self(), "You called dummy3"}.
 
